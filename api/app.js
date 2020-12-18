76b6979c4d078d7a22150e13103a502ccfe38aab
nosslsvec8 <nosiljevets@gmail.com>
2020-12-18 14:42:41 +0200

    Preparing ExpressJS:
    - add configuration file and its template
    - add check for config file presence
    
    - add check and error output when navigating to a non-existent page
    
    - add a command to run the project in developer mode

--- api/app.js
+++ api/app.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+const host = process.env.HOST;
+const fs = require('fs');
+const path = '.env';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', [
+  function (req, res, next) {
+    fs.access(path, fs.F_OK, (err) => {
+      if (err) {
+        console.log(`File ${path} not found`);
+        console.error(err);
+      }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+      console.log('Settings file found');
+      next();
+    });
+  },
+  function (req, res) {
+    res.send('Hello World!');
+  }
+]);
+
+// error handler - unknown url
+app.get('/*', (req, res) => {
+  res.status(404);
+
+  res.send({
+    error: 'Not found',
+    errorUrl: `${req.url}`
+  });
+});
+
+app.listen(port, host, () => {
+  console.log(`Server is listening at http://${host}:${port}`);
+});
