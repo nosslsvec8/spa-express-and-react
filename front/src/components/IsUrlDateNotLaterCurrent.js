e8fc791cf9846177e76cbb8c8412d0f77945c552
nosslsvec8 <nosiljevets@gmail.com>
2021-02-03 17:36:42 +0200

    Reworked routes for users in 1 route

--- front/src/components/IsUrlDateNotLaterCurrent.js
+++ front/src/components/IsUrlDateNotLaterCurrent.js
@@ -1,11 +1,11 @@
-function IsUrlDateNotLaterCurrent() {
+function IsUrlDateNotLaterCurrent(path) {
     const currentDate = new Date();
-    const relativePathSplitArr = window.location.href;
+    const relativePathSplit = path;
     const fullDateRegex = /(\d{4})-(0[0-9]|[1][0-2])-([0-2][0-9]|3[0-1])/;
     let urlDate = 0;
 
-    if(fullDateRegex.test(relativePathSplitArr)) {
-        urlDate = relativePathSplitArr.match(fullDateRegex);
+    if(fullDateRegex.test(relativePathSplit)) {
+        urlDate = relativePathSplit.match(fullDateRegex);
         urlDate =  new Date(urlDate[0]);
     }
 
--- front/src/components/Render.js
+++ front/src/components/Render.js
@@ -5,8 +5,8 @@ import Footer from "./Footer";
 import Articles from "./Articles";
 import ArticleAdd from "./ArticleAdd";
 import Profile from "./Profile";
+import User from "./User";
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
-import IsUrlDateNotLaterCurrent from "./IsUrlDateNotLaterCurrent";
 
 function Render() {
     return (
@@ -24,32 +24,16 @@ function Render() {
                     <Profile/>
                 </Route>
 
-                <Route exact path="/users">
-                    /users
-                </Route>
-                <Route exact path="/users/(\d+)">
-                    /users/(\d+)
-                </Route>
-                <Route exact path="/users/(\d+)">
-                    /users/:id
-                </Route>
-                <Route exact path="/users/(\d+)/edit">
-                    /users/:id/edit
-                </Route>
-                <Route exact path="/users/(\d+)/avatar">
-                    /users/:id/avatar
-                </Route>
-                <Route exact path="/users/(\d+)/avatar/edit">
-                    /users/:id/avatar/edit
-                </Route>
-                <Route exact path="/users/(\d+)/avatar/delete">
-                    /users/:id/avatar/delete
-                </Route>
-
-                <Route exact
-                     path="/users/(\d+)/file/(\d+)-(\w{1,10})-(\d{4})-(0[0-9]|[1][0-2])-([0-2][0-9]|3[0-1]).(docx|jpeg|pdf|txt)/v.(\d{1}).(\d{1}).(\d{1})"
-                >
-                    {(IsUrlDateNotLaterCurrent()) ? '/users/1/file/XXX-YYY-2020-01-20.FFF/v.Z.Z.Z' : <Redirect to="/articles"/>}
+                <Route exact path={[
+                    "/users",
+                    "/users/(\\d+)",
+                    "/users/(\\d+)/edit",
+                    "/users/(\\d+)/avatar",
+                    "/users/(\\d+)/avatar/edit",
+                    "/users/(\\d+)/avatar/delete",
+                    "/users/(\\d+)/file/(\\d+)-(\\w{1,10})-(\\d{4})-(0[0-9]|[1][0-2])-([0-2][0-9]|3[0-1]).(docx|jpeg|pdf|txt)/v.(\\d{1}).(\\d{1}).(\\d{1})",
+                ]}>
+                    <User/>
                 </Route>
 
                 <Redirect from="/" to="/articles"/>
