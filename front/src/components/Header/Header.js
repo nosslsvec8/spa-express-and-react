960bbfff701503f6fd5978093b26cec4388ffc58
nosslsvec8 <nosiljevets@gmail.com>
2021-03-21 23:29:46 +0200

    - fix extra characters

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -39,7 +39,7 @@ function Header() {
                                 <input type="email" name="email" id="email"/>
                             </div>
                             <div>
-                                Password: 11111111
+                                Password:
                                 <input type="password" name="password" id="password"/>
                             </div>
                             <div>
