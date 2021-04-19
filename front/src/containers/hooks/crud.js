1ebd99f921b6f71c6c2e83c264b87301836e6d63
nosslsvec8 <nosiljevets@gmail.com>
2021-04-19 21:10:35 +0300

    - remove unnecessary code duplication when adding a token to the header

--- front/src/containers/hooks/crud.js
+++ front/src/containers/hooks/crud.js
@@ -1,106 +1,66 @@
-import {apiClient} from "../../config/axios";
+import {defaultApiClient, secureApiClient} from "../../config/axios";
 
 export const getPosts = async (limit, page) => {
-    return (await apiClient.get(`/posts?page=${page}&limit=${limit}`));
+    return (await secureApiClient.get(`/posts?page=${page}&limit=${limit}`));
 };
 
 export const getCountPosts = async () => {
-    return (await apiClient.get('/posts/count'));
+    return (await defaultApiClient.get('/posts/count'));
 };
 
 export const getPost = async (id) => {
-    return (await apiClient.get(`/post/${id}`));
+    return (await secureApiClient.get(`/post/${id}`));
 };
 
 export const getUser = async (id) => {
-    return (await apiClient.get(`/user/${id}`));
+    return (await defaultApiClient.get(`/user/${id}`));
 };
 
 export const getByTokenUser = async (data) => {
     const accessToken = localStorage.getItem('accessToken');
-    if(accessToken !== null) {
-        return (await apiClient.post(`/getUser`, data, {
-            headers: {
-                'Authorization': `Bearer ${data.accessToken}`
-            }
-        }));
-    } else {
-        return false;
-    }
+
+    return (accessToken) ? await secureApiClient.post('/getUser', data) : false;
 };
 
 export const updateUserRequest = async (data) => {
-    const accessToken = localStorage.getItem('accessToken');
-    return (await apiClient.put(`/user/${data.id}`, data, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    }));
+    return (await secureApiClient.put(`/user/${data.id}`, data));
 };
 
 export const createPostRequest = async (data) => {
-    return (await apiClient.post('/post', data, {
-        headers: {
-            'Authorization': `Bearer ${data.accessToken}`
-        }
-    }));
+    return (await secureApiClient.post('/post', data));
 };
 
 export const updatePostRequest = async (data) => {
-    return (await apiClient.put(`/post/${data.id}`, data, {
-        headers: {
-            'Authorization': `Bearer ${data.accessToken}`
-        }
-    }));
+    return (await secureApiClient.put(`/post/${data.id}`, data));
 };
 
 export const deletePostRequest = async (data) => {
-    return (await apiClient.delete(`/post/${data.id}`, data, {
-        headers: {
-            'Authorization': `Bearer ${data.accessToken}`
-        }
-    }));
+    return (await secureApiClient.delete(`/post/${data.id}`, data));
 };
 
 export const loginRequest = async (data) => {
-    let accessToken = null;
-
-    await apiClient.post(`/auth/login`, data).then(response => {
-        accessToken = response?.data.accessToken;
+    await defaultApiClient.post('/auth/login', data).then(response => {
+        localStorage.setItem('accessToken', response?.data.accessToken);
     });
 
-    localStorage.setItem('accessToken', accessToken);
     window.location.reload();
 };
 
 export const socialLoginRequest = async (data) => {
-    let accessToken = null;
-
-    await apiClient.post(`/auth/social`, data).then(response => {
-        accessToken = response?.data.accessToken;
+    await defaultApiClient.post('/auth/social', data).then(response => {
+        localStorage.setItem('accessToken', response?.data.accessToken);
     });
 
-    localStorage.setItem('accessToken', accessToken);
     window.location.reload();
 };
 
 export const LogoutRequest = async () => {
     const accessToken = localStorage.getItem('accessToken');
 
-    await apiClient.post('/auth/logout', {accessToken: accessToken}, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
+    await secureApiClient.post('/auth/logout', {accessToken: accessToken});
 
     localStorage.removeItem('accessToken');
     window.location.reload();
 };
 
-export const isCheckAccessToken = async (data) => {
-    return (await apiClient.post(`/auth/isCheckAccessToken`, data, {
-        headers: {
-            'Authorization': `Bearer ${data.accessToken}`
-        }
-    }));
-};
+
